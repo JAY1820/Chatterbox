be4f1678396aa0725d831559848bc61422b2780e
jay1820 <iamjay1820@gmail.com>
2024-06-13 21:00:25 +0530

    Add main application styles and logic

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import {ChatEngine} from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
+import './App.css';
+const App=()=>{
+	return(
+
+			<ChatEngine
+			height="100vh"
+				projectID="1153e4f3-e3c2-4bc8-b99e-d3c33528e802"
+				userName="Master"
+				userSecret="masterid"
+				renderChatFeed={(chatAppProps)=><ChatFeed {...chatAppProps}/>}
+			/>
+		);
+}
+
+export default App;
